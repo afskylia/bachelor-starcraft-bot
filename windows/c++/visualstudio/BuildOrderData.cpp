3a21adeb9a633e516e11ab9470d5331c6edd8c26
Mikkel Anderson <mra@npstoday.com>
2021-04-15 15:03:08 +0200

    Data structure started

---
+++ windows/c++/visualstudio/BuildOrderData.cpp
@@ -0,0 +1,21 @@
+﻿#include "BuildOrderData.h"
+
+BuildOrderData::BuildOrderData()
+{
+}
+
+//8 - Pylon
+//10 - Gateway
+//12 - Assimilator
+//13 - Cybernetics Core
+build_order BuildOrderData::SimpleBuildOrder()
+{
+	build_order order;
+	order.steps = {
+		{8, BWAPI::UnitTypes::Protoss_Pylon},
+		{10, BWAPI::UnitTypes::Protoss_Gateway},
+		{12, BWAPI::UnitTypes::Protoss_Assimilator},
+		{13, BWAPI::UnitTypes::Protoss_Cybernetics_Core}
+	};
+	return order;
+}
---
+++ windows/c++/visualstudio/BuildOrderData.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "BWAPI.h"
+/**
+ * struct for storing one build step in a build order
+ */
+struct build_step
+{
+	int supply_level{};
+	BWAPI::UnitType unit;
+};
+
+/**
+ * struct for storing a build order
+ * if nothing else is noted in between two Supply marks - the player should build Probes and send them to mine
+ */
+struct build_order
+{
+	std::vector<build_step> steps;
+};
+
+class BuildOrderData
+{
+public:
+
+	BuildOrderData();
+	build_order SimpleBuildOrder();
+};
